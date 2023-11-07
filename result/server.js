0f162fe089e45bd377e03d39daaff5cb40d2740d
unknown <mayoujord@gmail.com>
2023-11-07 20:20:43 +0100

    add module result

--- result/server.js
+++ result/server.js
@@ -23,7 +23,7 @@ io.sockets.on("connection", function (socket) {
 })
 
 const pool = new pg.Pool({
-  connectionString: "postgres://postgres:postgres@postgres/postgres",
+  connectionString: "postgres://postgres:postgres@postgres/postgres", // change host from "localhost" to "postgres"
 })
 
 async.retry(
